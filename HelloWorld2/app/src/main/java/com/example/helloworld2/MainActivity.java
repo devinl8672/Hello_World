e1c2657275adfe6d98860a7da6a24332394710fd
Devin Lin <devin.lin@stonybrook.edu>
2022-01-21 23:58:12 -0500

    added in optional user stories

--- HelloWorld2/app/src/main/java/com/example/helloworld2/MainActivity.java
+++ HelloWorld2/app/src/main/java/com/example/helloworld2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = ((TextView) findViewById(R.id.text));
 
         // User can tap a button to change the text color of the label
         findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
@@ -35,6 +38,36 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // Change text color of label
                 ((TextView) findViewById(R.id.text)).setText(R.string.android_cheer);
+            }
+        });
+
+        //
+        //  Reset the text to Hello from Devin
+        //  Reset to color of the text to original color
+        //  Reset the background color
+        findViewById(R.id.background).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Change text color of label
+                textView.setText("Hello From Devin!");
+
+                textView.setTextColor(getResources().getColor(R.color.black));
+
+                findViewById(R.id.background).setBackgroundColor(getResources().getColor(R.color.cyan));
+            }
+        });
+
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userEnteredText = ((EditText) findViewById(R.id.editText)).getText().toString();
+
+                if (userEnteredText.isEmpty()) {
+                    textView.setText("Enter Your Own Text: ");
+                }
+                else {
+                    textView.setText(userEnteredText);
+                }
             }
         });
     }
